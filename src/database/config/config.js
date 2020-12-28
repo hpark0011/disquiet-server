01dd22393c557325daaa75fc83f035c7bac270fe
Jen H <jennyhong.jy@gmail.com>
2020-12-28 23:21:32 +0900

    Change database to postgres

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,11 @@
 export const config = {
   development: {
-    database: process.env.MYSQL_DATABASE,
-    username: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-    host: process.env.MYSQL_HOST,
-    port: 3306,
-    dialect: "mysql",
+    database: process.env.POSTGRES_DATABASE,
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: 5432,
+    dialect: "postgres",
     // connectTimeout: Number(process.env.CONNECT_TIMEOUT || 1000)
   }
 };
